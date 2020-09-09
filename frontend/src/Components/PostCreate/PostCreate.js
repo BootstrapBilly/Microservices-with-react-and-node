82706f42a3717e6f5bd137f87c6cf86496600394
Billy <billyjcatchpole@gmail.com>
2020-09-09 10:25:28 +0100

    Added basic react structure including a form which adds a new post via the posts service and axios

---
+++ frontend/src/Components/PostCreate/PostCreate.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import axios from "axios"
+
+const PostCreate = () => {
+
+    //*states
+    const [title, set_title] = useState("")
+
+    const handle_submit = async (e) => {
+
+        e.preventDefault()
+
+        const response = await axios.post("http://localhost:4000/posts", {
+
+            title: title
+
+        })
+
+        console.log(response)
+        set_title("")
+
+    }
+
+    return (
+
+        <form onSubmit={handle_submit}>
+
+            <div className={"form-group"}>
+
+                <label>Title</label>
+                <input className={"form-control w-25"} value={title} onChange={e => set_title(e.target.value)} />
+
+            </div>
+
+            <button className={"btn btn-primary"}>Submit</button>
+
+        </form>
+
+    )
+
+}
+
+export default PostCreate
